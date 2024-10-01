6baac53ebe7977a300c0cf643bf5e632986ae94d
Peter Klein <kleinpa00@gmail.com>
2024-10-01 03:51:42 -0400

    Hack up the entrypoint so the game starts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -ue
 
 # Install srcds required certificates
-apt-get update && apt-get install -y ca-certificates steamcmd cron
-
-# Attempt to update the game on first run
-steamcmd +force_install_dir /opt/game +login anonymous +app_update 1690800 validate +quit
+apt-get update && apt-get install -y ca-certificates
 
 # Mark srcds_linux as executable
 chmod +x /opt/game/FactoryServer.sh
+chmod +x /opt/game/Engine/Binaries/Linux/FactoryServer-Linux-Shipping
+
+useradd game
 
 # Start the server
-/opt/game/FactoryServer.sh
+su -c /opt/game/FactoryServer.sh game
+
