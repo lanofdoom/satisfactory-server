359799f17af6be00c6c88cb8de40d5ee259e2185
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2024-10-02 21:41:31 -0700

    Make writable additional directories required by Satisfactory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,29 @@
 #!/bin/bash -ue
 
-# Install sudo
-apt update && apt install -y sudo
+# Install certificates and sudo
+apt update && apt install -y ca-certificates sudo
 
 # Mark start script and server binary executable
 chmod +x /opt/game/Engine/Binaries/Linux/FactoryServer-Linux-Shipping
 chmod +x /opt/game/FactoryServer.sh
 
 # Create save directory and mark as writable for all users
-mkdir -p /opt/game/FactoryGame/Saved
-chmod -R 777 /opt/game/FactoryGame/Saved
+install -d -m 0777 -o nobody -g nogroup /data/FactoryGame/Saved
+install -d -m 0777 -o nobody -g nogroup /data/Engine/Saved
+install -d -m 0777 -o nobody -g nogroup /data/home
+
+# Create other writable directories needed by server
+install -d -m 0777 -o nobody -g nogroup /opt/game/FactoryGame/Intermediate
+
+# Create directories in game directory and link into /data
+install -d -m 0777 -o nobody -g nogroup /opt/game/FactoryGame/Saved
+install -d -m 0777 -o nobody -g nogroup /opt/game/Engine/Saved
+ln -s /data/FactoryGame/Saved /opt/game/FactoryGame/Saved
+ln -s /data/Engine/Saved /opt/game/Engine/Saved
+
+# Map nobody to the desired UID/GID
+usermod -u $PUID nobody
+groupmod -g $PGID nogroup
 
 # Start server as nobody
-sudo -u nobody /opt/game/FactoryServer.sh
+sudo -u nobody HOME=/data/home /opt/game/FactoryServer.sh
