557f9db51fb86a963973fb8408954a6f6dc2dfb0
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2024-10-02 22:55:13 -0700

    Change UID and GID earlier in the script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,19 @@
 # Install certificates and sudo
 apt update && apt install -y ca-certificates sudo
 
+# Map nobody to the desired UID/GID
+usermod -u $PUID nobody
+groupmod -g $PGID nogroup
+
 # Mark start script and server binary executable
 chmod +x /opt/game/Engine/Binaries/Linux/FactoryServer-Linux-Shipping
 chmod +x /opt/game/FactoryServer.sh
 
 # Create save directory and mark as writable for all users
-install -d -m 0777 -o nobody -g nogroup /data/FactoryGame/Saved
-install -d -m 0777 -o nobody -g nogroup /data/Engine/Saved
-install -d -m 0777 -o nobody -g nogroup /data/home
+mkdir -p /data/FactoryGame/Saved
+mkdir -p /data/Engine/Saved
+mkdir -p /data/home
+chown -R nobody:nogroup /data
 
 # Create other writable directories needed by server
 install -d -m 0777 -o nobody -g nogroup /opt/game/FactoryGame/Intermediate
@@ -20,10 +25,6 @@ install -d -m 0777 -o nobody -g nogroup /opt/game/FactoryGame/Saved
 install -d -m 0777 -o nobody -g nogroup /opt/game/Engine/Saved
 ln -s /data/FactoryGame/Saved /opt/game/FactoryGame/Saved
 ln -s /data/Engine/Saved /opt/game/Engine/Saved
-
-# Map nobody to the desired UID/GID
-usermod -u $PUID nobody
-groupmod -g $PGID nogroup
 
 # Start server as nobody
 sudo -u nobody HOME=/data/home /opt/game/FactoryServer.sh
